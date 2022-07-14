0ddddfc547fc6994104fa63e9842d1da8e2994d7
Geenensq <geenensq@gmail.com>
2022-07-14 16:02:13 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+Config.vehicleList = {
+    { name = "BMX", model = "bmx", price = 10},
+    { name = "Faggio", model = "faggio", price = 100 },
+    { name = "Blista", model = "blista", price = 500 },
+}
+
+Config.spawns = {
+    vector4(-1041.76, -2676.22, 13.41, 324.13),
+    vector4(-1031.88, -2658.51, 13.65, 150.12),
+    vector4(-1049.05, -2669.5, 13.65, 308.46),
+    vector4(-1040.39, -2653.34, 13.65, 148.59),
+}
