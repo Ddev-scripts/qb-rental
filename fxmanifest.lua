c845a4fdc42723fd301055bd22abf385dca0616d
Geenensq <geenensq@gmail.com>
2022-07-14 16:02:34 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+version '1.0.0'
+
+client_scripts {
+  'config.lua',
+  'client/client.lua',
+}
+
+server_scripts {
+  'config.lua',
+  'server/server.lua',
+}
